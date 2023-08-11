4f8546f150422d6d771ad6ddc4afdf1c030ded3e
Benii <benii.alco@gmail.com>
2023-08-11 22:36:05 +0100

    Parse Response Message
    Truly some of the worst parsing to have ever been written (going to replace the entire thing with a dedicated library though, so this should be taken as aggresively proof of concept! ~

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,17 @@ fn main() -> std::io::Result<()> {
         let mut buf = [0; 1024];
         match socket.recv(&mut buf) {
             Ok(received) => {
+                let idx = 12;
                 println!("{:x?}", &buf[..received]);
+                let header = &buf[..idx];
+                let h = DnsHeader::parse(header);
+                println!("{h:?}");
+                let question = &buf[12..];
+                let (idx, q) = DnsQuestion::parse(question);
+                println!("{q:x?}");
+                let answer = &buf[..received];
+                let a = DnsRecord::parse(answer, idx);
+                println!("{a:x?}");
                 Ok(())
             }
             Err(e) => Err(e),
@@ -19,21 +29,7 @@ fn main() -> std::io::Result<()> {
     }
 }
 
-// GOAL: Make a `Query` asking for the IP address for `google.com`
-// That is, what information do we need to make the request, and what format does it need to be in?
-//
-// A `DNS Query` has 2 parts:
-// 1. Header
-// 2. Question
-
-// A DNS header has:
-// - A `Query ID`
-// - Some `flags` (we'll ignore for now)
-// - 4 `counts`, enumerating the number of records to expect in each section of a `DNS Packet`:
-//      1. `num_questions`
-//      2. `num_answers`
-//      3. `num_authorities`
-//      4. `num_additionals`
+#[derive(Debug)]
 struct DnsHeader {
     id: u16,
     flags: u16,
@@ -60,12 +56,26 @@ impl DnsHeader {
         bytes.extend_from_slice(&num_additionals);
         bytes
     }
+
+    fn parse(data: &[u8]) -> Self {
+        let id = u16::from_be_bytes([data[0], data[1]]);
+        let flags = u16::from_be_bytes([data[2], data[3]]);
+        let num_questions = u16::from_be_bytes([data[4], data[5]]);
+        let num_answers = u16::from_be_bytes([data[6], data[7]]);
+        let num_authorities = u16::from_be_bytes([data[8], data[9]]);
+        let num_additionals = u16::from_be_bytes([data[10], data[11]]);
+        Self {
+            id,
+            flags,
+            num_questions,
+            num_answers,
+            num_authorities,
+            num_additionals,
+        }
+    }
 }
 
-// A DNS Question has:
-// - A `name`
-// - a `type`
-// - a `class`
+#[derive(Debug)]
 struct DnsQuestion {
     name: Vec<u8>,
     type_: u16,
@@ -96,6 +106,28 @@ impl DnsQuestion {
         bytes.extend_from_slice(&class_);
         bytes
     }
+
+    fn parse(data: &[u8]) -> (usize, Self) {
+        let mut parts = Vec::new();
+        let mut length = dbg!(data[0] as usize); //u16::from_be_bytes([data[0], data[1]);
+        let mut idx = 0;
+        while length != 0 {
+            idx += 1;
+            parts.extend_from_slice(&data[idx..idx + length]);
+            idx += length;
+            length = dbg!(data[idx] as usize);
+        }
+        let type_ = u16::from_be_bytes([data[idx + 1], data[idx + 2]]);
+        let class_ = u16::from_be_bytes([data[idx + 3], data[idx + 4]]);
+        (
+            idx,
+            Self {
+                name: parts,
+                type_,
+                class_,
+            },
+        )
+    }
 }
 
 // TODO: implement class/record/flags/id
@@ -114,4 +146,64 @@ fn build_query(domain_name: &str) -> Vec<u8> {
     let mut query = header.as_bytes();
     query.append(&mut question.as_bytes());
     query
+}
+
+// GOAL: `parse_response` function that parses the DNS Query response into a struct
+
+// A `DnsRecord` has:
+// - name: The domain name
+// - type_: A/AAAA/MX/NS/TXT/...
+// - class_: Assume will always be 1
+// - ttl: Time To Live; how long the query ought be cached for
+// - data_length: How many octets the data takes up
+// - data: the record's content
+#[derive(Debug)]
+struct DnsRecord {
+    name: Vec<u8>,
+    type_: u16,
+    class_: u16,
+    ttl: u32,
+    data_length: u16,
+    data: Vec<u8>,
+}
+
+impl DnsRecord {
+    fn parse(data: &[u8], idx: usize) -> Self {
+        println!("{:x?}", data);
+        let length = data[idx + 17];
+        dbg!(length);
+        if length > 63 {
+            let pointer = (length & 0b0011_1111) as usize;
+            let data = &data[pointer..];
+            let mut parts = Vec::new();
+            let mut length = dbg!(data[12] as usize); //u16::from_be_bytes([data[0], data[1]);
+            let mut i = 12;
+            while length != 0 {
+                i += 1;
+                parts.extend_from_slice(&data[i..i + length]);
+                i += length;
+                length = dbg!(data[i] as usize);
+            }
+            let type_ = u16::from_be_bytes([data[idx + 1 + 18], data[idx + 2 + 18]]);
+            let class_ = u16::from_be_bytes([data[idx + 3 + 18], data[idx + 4 + 18]]);
+            let ttl = u32::from_be_bytes([
+                data[idx + 5 + 18],
+                data[idx + 6 + 18],
+                data[idx + 7 + 18],
+                data[idx + 8 + 18],
+            ]);
+            let data_length = u16::from_be_bytes([data[idx + 9 + 18], data[idx + 10 + 18]]);
+            let data = data[idx + 11 + 18..].to_vec();
+            DnsRecord {
+                name: parts,
+                type_,
+                class_,
+                ttl,
+                data_length,
+                data,
+            }
+        } else {
+            panic!()
+        }
+    }
 }
